47f2525f7b46a8ddf138e5ec4170fc5bb6cf8877
Kritpong Smith <kritpong@me.com>
2017-10-06 15:56:43 +0700

    text chat

--- bot.php
+++ bot.php
@@ -18,8 +18,15 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+			
+			        {
+			            "type":"อีจ่อย",
+			            "text":"มาแล้วจร้าาา"
+			        },
+			        {
+			            "type":"อีฟาย",
+			            "text":"แกสิฟาย"
+			        }
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
