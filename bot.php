4333b6defafd4d4ce78f8992c188a660c657be25
Kritpong Smith <kritpong@me.com>
2017-10-08 18:38:12 +0700

    update

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
       // Build message to reply back
       $messages = [
         'type' => 'แม่จ่อย',
-        'text' => ว่าไง
+        'text' => 'ว่าไง'
       ];
 
       // Make a POST Request to Messaging API to reply to sender
